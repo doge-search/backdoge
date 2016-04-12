335b3b7215eeee73c72cebca6110de8e28ad36ec
Seter Guo <seter@pku.edu.cn>
2016-04-12 16:31:05 +0800

    init prof

---
+++ common.py
@@ -0,0 +1,67 @@
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+if not config.read('db.cfg'):
+	raise Exception("Missing db.cfg")
+
+DB_USERNAME = config.get("mysql", "username")
+DB_PASSWORD = config.get("mysql", "password")
+DB_DATABASE = config.get("mysql", "database")
+
+import torndb
+
+db = torndb.Connection(
+		"localhost",
+		DB_DATABASE,
+		DB_USERNAME,
+		DB_PASSWORD
+		)
+
+DB_INIT_TABLE = {
+		"doge_prof": [
+"drop table if exists doge_prof",
+"""CREATE TABLE `doge_prof` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`school` varchar(255) NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`title` varchar(255) DEFAULT NULL,
+	`office` varchar(255) DEFAULT NULL,
+	`email` varchar(255) DEFAULT NULL,
+	`phone` varchar(255) DEFAULT NULL,
+	`website` varchar(255) DEFAULT NULL,
+	`image` varchar(255) DEFAULT NULL,
+	`group` varchar(255) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8"""
+			]
+		}
+
+DB_INSERT_TABLE = {
+		"doge_prof": [
+			"school", "name", "title", "office", "email",
+			"phone", "website", "image", "group"
+			]
+		}
+
+def insert_table(table_name, params):
+	arg_list = DB_INSERT_TABLE[table_name]
+	sql = "insert into %s (%s) values (%s)" % (
+			table_name,
+			", ".join(["`%s`" % arg for arg in arg_list]),
+			", ".join(["%s"] * len(arg_list))
+			)
+
+	db.insertmany(sql,
+			[
+				[
+					elem[t] if t in elem else ''
+					for t in arg_list
+					]
+				for elem in params
+				]
+			)
+
+def init_table(table_name):
+	print "Initializing table: %s" % table_name
+	for sql in DB_INIT_TABLE[table_name]:
+		db.execute(sql)
---
+++ init_prof.py
@@ -0,0 +1,47 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+		description = 'Convert professors from XML to MySQL.'
+		)
+group = parser.add_mutually_exclusive_group(required = True)
+group.add_argument('-a',
+		action = 'store',
+		dest = 'DIR',
+		help='append a directory')
+group.add_argument('-c',
+		action = 'store_true',
+		dest = 'RESET',
+		help='reset database')
+args = parser.parse_args()
+
+import common
+from common import db
+
+if args.RESET:
+	common.init_table("doge_prof")
+	exit(0)
+
+import os
+try:
+	import xml.etree.cElementTree as ET
+except ImportError:
+	import xml.etree.ElementTree as ET
+
+for node in os.listdir(args.DIR):
+	path = os.path.join(args.DIR, node)
+	if not os.path.isdir(path):
+		continue
+	filename = os.path.join(path, node + ".xml")
+	try:
+		tree = ET.ElementTree(file = filename)
+	except IOError:
+		print "File %s not found!" % filename
+		continue
+	print "Converting %s..." % filename
+	result = []
+	for prof in tree.getroot():
+		d = {"school": node}
+		for info in prof:
+			d[info.tag] = info.text
+		result.append(d)
+	common.insert_table("doge_prof", result)
